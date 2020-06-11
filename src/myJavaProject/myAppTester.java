394f06b77726c4318850248357f3352288b5d705
lucifer <dubeys@student.douglascollege.ca>
2020-06-11 03:36:16 -0700

    This File is commited by dubey-s4233

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -5,10 +5,10 @@ package myJavaProject;
 
 import java.util.Scanner;
 public class myAppTester {
-	
+	static Scanner input = new Scanner(System.in);
  public static void main(String[] args) {
 	  
-	  Scanner input = new Scanner(System.in);
+	  
 	  int n=0;
 	  System.out.println("Hello Software Engineering World");
 	  System.out.println("Input your number and press enter: ");
@@ -16,6 +16,9 @@ public class myAppTester {
 	  System.out.print("Reverse of the input number is:");
 	  reverseMethod(n);
 	  System.out.println();
+
+	  String message =  Message4233();
+		System.out.print("Your Message is : "+ message);
 
 	  // add a line below to call your myTestMethod from here  then remove the block comment symbols  
 
@@ -32,7 +35,20 @@ public class myAppTester {
 	           reverseMethod(num/10);     // recursive call
 	       }
 	   }
+			
+		
+		
+		
+	}
 
+	public static String Message4233()
+	{
+		System.out.println("Enter a message Please");
+		String msg= input.nextLine();
+		
+		return msg;
+		
+	}
 
 	   /* 
 	       add your test method in this space then remove the block comment symbols
